baf8beada506754434b4efaead2608b2ed7c41ac
Mitchell Frisch <mitchellfrisch1@gmail.com>
2024-12-27 20:45:38 +1000

    main.c file

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+	/* 
+	 * Server Side Socket Process 
+	 * 1. Create socket 
+	 * 2. Bind socket
+	 * 3. Listen to oncoming request
+	 * 4. Accept or reject to request
+	 * 5. Communicate using socket 
+	 * 6. Close Connection
+	 */
+	return 0;
+}
