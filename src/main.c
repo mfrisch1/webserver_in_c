735b1b371bd1695821966365172c58814ca3ddab
mfrisch1 <mitchellfrisch1@gmail.com>
2025-01-04 07:56:56 +1000

    Finished basic server side code for socket

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define PORT 8080 // Client will send data to this port
 
@@ -31,7 +34,47 @@ int main() {
 	// using bind system call
 	// bind(int socket, const struct sockaddr *address, socklen_t address_len);
 	// sockaddr							generic container, use struct sockaddr_in from netinet/in.h for IP networking 
-	struct sockaddr_in addr;
+	struct sockaddr_in addr; // IPv4 addr structure
+	memset((char *)&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	// INADDR_ANY						(0.0.0.0), means any address for socket binding
+	addr.sin_addr.s_addr = htonl(INADDR_ANY); // host to network type long int
+	addr.sin_port = htons(PORT);
+
+	if (bind(tcp_socket_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("Bind failed");
+		return 0;
+	}
+
+	// listen for incoming connection
+	if (listen(tcp_socket_fd, 3) < 0) {
+		perror("In listen");
+		exit(EXIT_FAILURE);
+	}
+
+	// Create new socket fd for accepted request.
+	int new_tcp_socket_fd;
+	int addrlen = sizeof(addr);
+	if ((new_tcp_socket_fd = accept(tcp_socket_fd, (struct sockaddr *)&addr, (socklen_t *)&addrlen)) < 0) {
+		// 2nd paramater: Address structure that gets filed in with the address of the client that is doing the connect.
+		perror("In accept");
+		exit(EXIT_FAILURE);
+	}
+
+	// Send and Recieve message
+	// since socket is a file desciptor to a file, write and read system call that works on file works on sockets
+	char buff[1024] = {0};
+	int valread = read( new_tcp_socket_fd, buff, 1024);
+	printf("%s\n", buff);
+	if (valread < 0) {
+		printf("No byte are there to read");
+	}
 	
+	char *hello = "Hello from the server!";
+	write(new_tcp_socket_fd, hello, strlen(hello));
+	
+	// close the connection
+	close(new_tcp_socket_fd);
+
 	return 0;
 }
